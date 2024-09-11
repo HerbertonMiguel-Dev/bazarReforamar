3529eee726f3784f620c6b5772bf19c53376a6ee
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-09-11 17:11:55 -0300

    feito o função de buscar produto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
 import { Link } from "react-router-dom";
-import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { collection, query, getDocs, orderBy,where } from "firebase/firestore";
 import { db } from "../../services/firebaseConnection";
 
 interface ProdsProps {
@@ -25,9 +25,13 @@ interface ProdImageProps {
 export function Home() {
   const [prods, setProds] = useState<ProdsProps[]>([]);
   const [loadImages, setLoadImages] = useState<string[]>([])
+  const [input, setInput] = useState("")
 
   useEffect(() => {
-    function loadProds() {
+    loadProds();
+  }, []);
+
+  function loadProds() {
       const prodsRef = collection(db, "produtos");
       const queryRef = query(prodsRef, orderBy("created", "desc"));
 
@@ -52,11 +56,44 @@ export function Home() {
       });
     }
 
-    loadProds();
-  }, []);
-
   function handleImageLoad(id: string){
     setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+  }
+
+  async function handleSearchProd(){
+    if(input === ''){
+      loadProds();
+      return;
+    }
+
+    setProds([]);
+    setLoadImages([]);
+
+    const q = query(collection(db, "produtos"), 
+    where("name", ">=", input.toUpperCase()),
+    where("name", "<=", input.toUpperCase() + "\uf8ff")
+    )
+
+    const querySnapshot = await getDocs(q)
+
+    let listprods = [] as ProdsProps[];
+
+    querySnapshot.forEach((doc) => {
+      listprods.push({
+        id: doc.id,
+          name: doc.data().name,
+          categoria: doc.data().categoria,
+          medqua: doc.data().medqua,
+          description: doc.data().description,
+          forpag: doc.data().forpag,
+          price: doc.data().price,
+          images: doc.data().images,
+          uid: doc.data().uid,
+      })
+    })
+
+   setProds(listprods);
+
   }
 
   return (
@@ -64,9 +101,14 @@ export function Home() {
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
         <input
           className="w-full border-2 rounded-lg h-9 px-3 outline-none"
-          placeholder="Digite o nome do carro..."
+          placeholder="Digite o nome do Produto..."
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
         />
-        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+        <button 
+          className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+          onClick={handleSearchProd}
+        >
           Buscar
         </button>
       </section>
@@ -95,7 +137,7 @@ export function Home() {
 
               <div className="flex flex-col px-2">
                 <span className="text-zinc-700 mb-6">{prod.categoria}</span>
-                <strong className="text-black font-medium text-xl">
+                <strong className="text-black font-medium text-xl">R$
                   {prod.price} 
                   <span className="text-sm text-zinc-500 font-normal ml-2">{prod.forpag}</span>
                 </strong>
